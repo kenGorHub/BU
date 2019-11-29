9b409206b42c05a5981c83417553d1a2b2dac0f0
kenGorHub <46789334+kenGorHub@users.noreply.github.com>
2019-11-30 07:04:38 +0800

    debuged but the math not match to requirement

--- ken/ANN.cpp
+++ ken/ANN.cpp
@@ -45,7 +45,7 @@ void Neuron::updateInputWeights(Layer &prevLayer){
 	for(unsigned n = 0; n < prevLayer.size(); ++n){
 		Neuron &neuron = prevLayer[n];
 		double oldDeltaWeight = neuron.m_outputWeights[m_myIndex].deltaWeight;
-		double newDeltaWeight = eta * neuron_getOutputVal() * m_gradient + alpha * oldDeltaWeight;
+		double newDeltaWeight = eta * neuron.getOutputVal() * m_gradient + alpha * oldDeltaWeight;
 		
 		
 		neuron.m_outputWeights[m_myIndex].deltaWeight = newDeltaWeight;
@@ -66,19 +66,19 @@ double Neuron::sumDOW(const Layer &nextLayer)const {
 
 void Neuron::calcHiddenGradients(const Layer &nextLayer){
 	double dow = sumDOW(nextLayer);
-	m_gradient = dow * Neuron::transferFunctionDerivate(m_outputVal);
+	m_gradient = dow * Neuron::transferFunctionDerivative(m_outputVal);
 }
 
 void Neuron::calcOutputGradients(double targetVal){
 	double delta = targetVal - m_outputVal;
-	m_gradient = delta * Neuron::transferFunctionDerivate(m_outputVal);
+	m_gradient = delta * Neuron::transferFunctionDerivative(m_outputVal);
 }
 
 double Neuron::transferFunction(double x){
 	return tanh(x);
 }
 
-double Neuron::transferFunctionDerivate(double x){
+double Neuron::transferFunctionDerivative(double x){
 	return 1.0 - x * x;
 }
 
@@ -130,25 +130,35 @@ void Net::backProp(const vector<double> &targetVals){
 	
 	for(unsigned n = 0; n < outputLayer.size() - 1;++n){
 		double delta = targetVals[n] - outputLayer[n].getOutputVal();
-		m_error += delta * delta;
+		m_error += delta * delta; 
 	}
-	m_error /= outputLayer.size() - 1;
-	m_error = sqrt(m_error);
+	m_error /= outputLayer.size() - 1; // get average error squared
+	m_error = sqrt(m_error);// RMS
+	
+	//Implement a recent average measurement
 	
 	m_recentAverageError = (m_recentAverageError * m_recentAverageSmoothingFactor + m_error)/(m_recentAverageSmoothingFactor + 1.0);
+	
+	//Calculate output layer gradients
 	
 	for(unsigned n = 0; n < outputLayer.size() - 1; ++n){
 		outputLayer[n].calcOutputGradients(targetVals[n]);
 	}
+	
+	
+	//Calculate gradients on hiddenLayer 
 	
 	for(unsigned layerNum = m_layers.size() - 2;layerNum > 0;--layerNum){
 		Layer &hiddenLayer = m_layers[layerNum];
 		Layer &nextLayer = m_layers[layerNum + 1];
 		
 		for(unsigned n = 0; n < hiddenLayer.size(); ++n){
-			hiddenLayer[n].calcOutputGradients(nextLayer);
+			hiddenLayer[n].calcHiddenGradients(nextLayer);
 		}
 	}
+	
+	//For all layers from outputs to first hidden layer,
+	//update connection weights
 	
 	for(unsigned layerNum = m_layers.size() - 1; layerNum > 0; --layerNum){
 		Layer &layer = m_layers[layerNum];
@@ -158,15 +168,19 @@ void Net::backProp(const vector<double> &targetVals){
 			layer[n].updateInputWeights(prevLayer);
 		}
 	}
+	
+	
 }
 
 void Net::feedForward(const vector<double> &inputVals){
 	assert(inputVals.size() == m_layers[0].size() - 1);
 	
+	// Assign (latch) the input values into the input neurons
 	for(unsigned i = 0;i < inputVals.size(); ++i){
 		m_layers[0][i].setOutputVal(inputVals[i]);
 	}
 	
+	//forward propagate
 	for(unsigned layerNum = 1; layerNum < m_layers.size(); ++layerNum){
 		Layer &prevLayer = m_layers[layerNum - 1];
 		for(unsigned n = 0;n < m_layers[layerNum].size() - 1; ++n){
